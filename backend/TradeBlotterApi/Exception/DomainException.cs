b597045179c7bff53a5d683c4e0f6c8a636c6341
ravip7237 <ravi.pran@gmail.com>
2026-06-29 04:58:22 +0000

    Deleted DomainException class as its not required

--- backend/TradeBlotterApi/Exception/DomainException.cs
+++
@@ -1,9 +0,0 @@
-namespace TradeBlotterApi.ExceptionHandling;
-
-public abstract class DomainException : Exception
-{
-    protected DomainException(string message)
-        : base(message)
-    {
-    }
-}
